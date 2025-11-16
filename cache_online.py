f29a20557aaf186e36b0509298413a2017d4810a
aqjshi <85421203+aqjshi@users.noreply.github.com>
2025-11-16 16:20:54 +0000

    Update cache_online.py

--- cache_online.py
+++ cache_online.py
@@ -1,1 +1,14 @@
 
+import pandas as pd
+import pyarrow 
+import sys
+
+parquet_file = "NEWS_20240101-142500_20251101-232422.parquet"
+
+df = pd.read_parquet(parquet_file)
+
+
+df_sorted = df.sort_values(by='time_published_ts', ascending=True)
+
+print(df_sorted["url"].head())
+
